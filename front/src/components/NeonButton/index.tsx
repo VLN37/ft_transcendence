eb70be0bd12ac36eb18e4ff20b8c94e51a15ea24
Paulo Santana <paulo.santana.r@gmail.com>
2022-09-22 23:46:48 -0300

    feat: add a simple neon button

---
+++ front/src/components/NeonButton/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import './style.css'
+
+type NeonButtonProps = {
+    children?: React.ReactNode;
+}
+
+function NeonButton({ children }: NeonButtonProps) {
+    return (
+        <button className="neon-border neon-text">{children}</button>
+    )
+}
+
+export default NeonButton
