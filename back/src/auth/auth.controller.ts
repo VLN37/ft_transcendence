c2f6a64470124ec6a5c0bec16d46418a5039f5f4
wleite <wleite@student.42sp.org.br>
2022-09-29 21:22:52 -0300

    refactor: rename auth callback var name

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get, Query } from '@nestjs/common';
 @Controller()
 export class AuthController {
   @Get('auth-callback')
-  auth(@Query('code') que: any) {
-    return que;
+  auth(@Query('code') code: any) {
+    return code;
   }
 }
