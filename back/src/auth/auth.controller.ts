337a57e4081aa9262df1d309bb408ee11426e7a3
wleite <wleite@student.42sp.org.br>
2022-09-29 21:35:41 -0300

    feat: auth func

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller()
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Get('auth-callback')
   auth(@Query('code') code: any) {
-    return code;
+    return this.authService.auth(code);
   }
 }
--- back/src/auth/auth.service.ts
+++ back/src/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  auth(code: number) {
+    return code;
+  }
+}
