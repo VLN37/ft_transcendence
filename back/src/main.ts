79006a228b96d8b3df60750feb5d82a1851bc1d4
wleite <wleite@student.42sp.org.br>
2022-10-05 14:04:50 -0300

    feat: add swagger

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,12 +1,25 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-  }));
+
+  const config = new DocumentBuilder()
+    .setTitle('ft_transcendence')
+    .setDescription('The ft_transcendence API')
+    .setVersion('1.0')
+    .addTag('ft_transcendence')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
