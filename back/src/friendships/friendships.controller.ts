1a5f873502c61cb0fedd1627c8ca04d4a3a733fe
wleite <wleite@student.42sp.org.br>
2022-10-06 14:56:20 -0300

    refactor: change friends route methods

--- back/src/friendships/friendships.controller.ts
+++ back/src/friendships/friendships.controller.ts
@@ -1,21 +1,21 @@
-import { Body, Controller, Delete, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Post, Put } from '@nestjs/common';
 import { FriendshipsService } from './friendships.service';
 
 @Controller('friends')
 export class FriendshipsController {
   constructor(private readonly friendshipsService: FriendshipsService) {}
 
-  @Post(':id/add')
+  @Post(':id')
   async add(@Param('id') id: any, @Body() user: any) {
     return await this.friendshipsService.add(id, user.user_id);
   }
 
-  @Delete(':id/remove')
+  @Delete(':id')
   async remove(@Param('id') id: any, @Body() user: any) {
     return await this.friendshipsService.remove(id, user.user_id);
   }
 
-  @Post(':id/accept')
+  @Put(':id')
   async accept(@Param('id') id: any, @Body() user: any) {
     return await this.friendshipsService.accept(id, user.user_id);
   }
