e294a8a3d831aca0a51a3f9dc5e6333575e138d8
Paulo Santana <paulo.santana.r@gmail.com>
2022-10-09 18:04:07 -0300

    chore: simulate loading time on home page

--- front/src/pages/home/index.tsx
+++ front/src/pages/home/index.tsx
@@ -1,3 +1,25 @@
+import { useEffect, useState } from 'react';
+
+interface Dashboard {
+  id: number;
+}
+
 export default function HomePage() {
-  return <div className="page">this is the home page</div>;
+  const [data, setData] = useState<Dashboard | null>(null);
+
+  useEffect(() => {
+    async function getDataSummary() {
+      setTimeout(() => {
+        setData({ id: 42 });
+      }, 2000);
+    }
+
+    getDataSummary();
+  }, []);
+
+  return (
+    <div className="page">
+      {data ? <div>waited succesfully</div> : <div>waiting 2000 ms</div>}
+    </div>
+  );
 }
