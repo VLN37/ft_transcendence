fac37ab59b9860c0b94da27d3358e32025937424
VLN37 <jofelipe@student.42sp.org.br>
2022-10-12 17:49:58 -0300

    "fix: user signup validation"

--- back/src/main.ts
+++ back/src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
+      transform: true,
     }),
   );
   await app.listen(3000);
--- back/src/users/dto/user.dto.ts
+++ back/src/users/dto/user.dto.ts
@@ -1,10 +1,16 @@
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString } from 'class-validator';
 import { Profile } from 'src/entities/profile.entity';
 
 export class UserDto {
+  @IsNumber()
   @IsNotEmpty()
   id: number;
 
+  @IsString()
   @IsNotEmpty()
   login_intra: string;
 
