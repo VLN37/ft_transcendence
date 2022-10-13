27fdc656e538ee19fdc4d89672ff7c14c24146e5
wleite <wleite@student.42sp.org.br>
2022-10-13 15:39:53 -0300

    refactor: remove unused import

--- back/src/auth/strategy/jwt.strategy.ts
+++ back/src/auth/strategy/jwt.strategy.ts
@@ -2,11 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { ExtractJwt } from 'passport-jwt';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(config: ConfigService) {
+  constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
