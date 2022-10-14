a08d5460b9c9105409a6fd77e97a8f4c956005e6
wleite <wleite@student.42sp.org.br>
2022-10-14 19:41:19 -0300

    feat: add post route to block users

--- back/src/blocked/blocked.controller.ts
+++ back/src/blocked/blocked.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { BlockedService } from './blocked.service';
 
-@Controller('/users/:id/blocked_users')
+@Controller('/users/:from/blocked_users')
 export class BlockedController {
   constructor(private readonly blockedService: BlockedService) {}
 
   @Get()
-  async get(@Param('id') id: number) {
-    return await this.blockedService.get(id);
+  async get(@Param('from') from: number) {
+    return await this.blockedService.get(from);
+  }
+
+  @Post()
+  async block(@Param('from') from: number, @Body('user_id') to: number) {
+    return await this.blockedService.block(from, to);
   }
 }
--- back/src/blocked/blocked.service.ts
+++ back/src/blocked/blocked.service.ts
@@ -1,13 +1,44 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class BlockedService {
+  private readonly logger = new Logger(UsersService.name);
+
   constructor(private usersService: UsersService) {}
 
-  async get(id: number) {
-    const user = await this.usersService.findOne(id);
+  async get(from: number) {
+    const user = await this.usersService.findOne(from);
     if (user) return user.blocked;
     throw new NotFoundException('User not found');
+  }
+
+  async block(from: number, to: number) {
+    const user = await this.usersService.findOne(from);
+    if (!user) throw new NotFoundException('User not found');
+
+    if (user.id == to)
+      throw new BadRequestException("You can't block yourself");
+
+    const userToBlock = await this.usersService.findOne(to);
+    if (!userToBlock) throw new NotFoundException('User to block not found');
+
+	this.logger.error(JSON.stringify(userToBlock));
+
+    if (user.blocked.find((userToBlock) => userToBlock.id == to))
+      throw new BadRequestException('User has already been blocked');
+
+    user.blocked.push(userToBlock);
+
+    this.logger.log(
+      `User ${user.login_intra} blocked user ${userToBlock.login_intra}`,
+    );
+
+    return await this.usersService.update(user);
   }
 }
