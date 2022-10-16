a5cc8ebe84479432f8a60c68249458199f019271
Paulo Santana <paulo.santana.r@gmail.com>
2022-10-16 15:05:05 -0300

    refactor(front): use chakra's Box element for main container

--- front/src/pages/login/index.tsx
+++ front/src/pages/login/index.tsx
@@ -1,12 +1,14 @@
+import { Box } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import NeonButton from '../../components/NeonButton';
 
 export default function LoginPage({ user }: any) {
   const authorizationUrl = process.env.REACT_APP_INTRA_AUTHORIZATION_URL;
-  console.log({ authorizationUrl });
-  console.log({ env: process.env.NODE_ENV });
+
+  console.log('enviroment: ' + process.env.NODE_ENV);
+
   return (
-    <div className="main-container">
+    <Box paddingX="10%">
       <h2>Welcome to PONG Transcendence</h2>
       <p>Log in on 42 Intranet to access the game</p>
       {user ? (
@@ -18,6 +20,6 @@ export default function LoginPage({ user }: any) {
           <NeonButton>LOGIN</NeonButton>
         </a>
       )}
-    </div>
+    </Box>
   );
 }
