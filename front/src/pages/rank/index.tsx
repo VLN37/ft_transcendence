d1dd92b281f6f8c8fcea170f63f7aa69be629221
Paulo Santana <paulo.santana.r@gmail.com>
2022-10-23 23:52:31 -0300

    feat: filter rank as the user types

--- front/src/pages/rank/index.tsx
+++ front/src/pages/rank/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import {
   Box,
   Button,
@@ -6,49 +6,42 @@ import {
   InputGroup,
   InputRightElement,
   IconButton,
-} from "@chakra-ui/react";
-import {
-  SearchIcon
-} from "@chakra-ui/icons";
-import {
-    RankTable,
-} from "../../components/Rank/";
-
+} from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
+import { RankTable } from '../../components/Rank/';
 
 export default function RankPage() {
   const [searchValue, setSearchValue] = useState('');
-  const [Search, setSearch] = useState('');
-  function SearchForm() {
+  function handleSearchClick() {
     console.log('value: ', searchValue);
-    setSearch(searchValue);
-    setSearchValue('');
+    setSearchValue(searchValue);
   }
 
   const handleChange = (event: any) => setSearchValue(event.target.value);
 
   return (
-  <div>
-  <Box marginLeft={'315px'} width='60%'>
-    <InputGroup>
-      <Input
-        placeholder='Search by login name'
-        value={searchValue}
-        onChange={handleChange}
-        size='md'/>
-      <InputRightElement
-        children={
-          <SearchIcon
-            color='teal.600'
-            onClick={() => SearchForm()}/>}
-      />
-    </InputGroup>
-  </Box>
-  <Box marginLeft={'315px'} width='60%' bg='cyan.800'>
-      {/* <Button>submit</Button> */}
-      <div className="page" >
-          <RankTable query={Search}/>
-      </div>
-  </Box>
-  </div>
+    <div>
+      <Box marginLeft={'315px'} width="60%">
+        <InputGroup>
+          <Input
+            placeholder="Search by login name"
+            value={searchValue}
+            onChange={handleChange}
+            size="md"
+          />
+          <InputRightElement
+            children={
+              <SearchIcon color="teal.600" onClick={handleSearchClick} />
+            }
+          />
+        </InputGroup>
+      </Box>
+      <Box marginLeft={'315px'} width="60%" bg="cyan.800">
+        {/* <Button>submit</Button> */}
+        <div className="page">
+          <RankTable query={searchValue} />
+        </div>
+      </Box>
+    </div>
   );
 }
