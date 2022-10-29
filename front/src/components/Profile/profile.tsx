bfcedaf0702d78dc6f5e73a7a0acee8f6abf7e9b
VLN37 <jofelipe@student.42sp.org.br>
2022-10-29 10:45:06 -0300

    feat: profile modal prototype

--- front/src/components/Profile/profile.tsx
+++ front/src/components/Profile/profile.tsx
@@ -1,17 +1,43 @@
-import { Image } from '@chakra-ui/react'
+import {
+  Image,
+  Modal,
+  Button,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+  useDisclosure,
+} from '@chakra-ui/react'
 
 export function Profile() {
-  const link = JSON.parse(localStorage.getItem('user') || '')
-                   .profile.avatar_path || '';
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const link =
+    JSON.parse(localStorage.getItem('user') || '').profile.avatar_path || '';
   return (
     <div>
-    <Image
+    <Image onClick={onOpen}
       marginTop={'15px'}
       borderRadius="full"
       boxSize="65px"
       src={link}
     />
+    <Modal isOpen={isOpen} onClose={onClose} size={"xl"}>
+        <ModalOverlay />
+        <ModalContent>
+          {/* <ModalCloseButton></ModalCloseButton> */}
+          <ModalHeader>Profile</ModalHeader>
+          <ModalBody>
+            <p>mimimi</p>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>close</Button>
+          </ModalFooter>
+        </ModalContent>
+    </Modal>
     </div>
+
   )
 }
 
