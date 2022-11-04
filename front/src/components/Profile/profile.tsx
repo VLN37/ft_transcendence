4ea24d058d02198a25ec005a274514a700873323
VLN37 <jofelipe@student.42sp.org.br>
2022-11-04 17:51:58 -0300

    refactor: profile stuff

--- front/src/components/Profile/profile.tsx
+++ front/src/components/Profile/profile.tsx
@@ -1,3 +1,4 @@
+import { AttachmentIcon, CheckIcon, DownloadIcon } from '@chakra-ui/icons';
 import {
   Image,
   Modal,
@@ -12,7 +13,36 @@ import {
   Grid,
   GridItem,
   Switch,
-} from '@chakra-ui/react'
+  Input,
+  InputGroup,
+  InputRightElement,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+
+function SendButton() {
+  return <Button></Button>;
+}
+
+function InputFileUpload() {
+  const [value, setValue] = useState('');
+  const handleChange = (event: any) => setValue(event.target.value);
+
+  const fileUpload = () => console.log('uploading ', value);
+
+  return (
+    <InputGroup size="md">
+      <Input
+        value={value}
+        onChange={handleChange}
+        type="file"
+        accept=".jpg, .png"
+      ></Input>
+      <InputRightElement
+        children={<DownloadIcon onClick={fileUpload} />}
+      ></InputRightElement>
+    </InputGroup>
+  );
+}
 
 export function Profile() {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -20,34 +50,44 @@ export function Profile() {
     JSON.parse(localStorage.getItem('user') || '').profile.avatar_path || '';
   return (
     <div>
-    <Image onClick={onOpen}
-      marginTop={'15px'}
-      borderRadius="full"
-      boxSize="65px"
-      src={link}
-    />
-    <Modal isOpen={isOpen} onClose={onClose} size={"3xl"}>
+      <Image
+        onClick={onOpen}
+        marginTop={'15px'}
+        borderRadius="full"
+        boxSize="65px"
+        src={link}
+      />
+      <Modal isOpen={isOpen} onClose={onClose} size={'3xl'}>
         <ModalOverlay />
         <ModalContent>
           <ModalCloseButton></ModalCloseButton>
           <ModalHeader>Profile</ModalHeader>
           <ModalBody>
+            <Image src={link}></Image>
             <Grid templateColumns={'repeat(5, 1 fr)'}>
-              <GridItem colStart={1} >Nickname</GridItem>
-              <GridItem colStart={5}><Button>change</Button></GridItem>
+              <GridItem colStart={1}>Nickname</GridItem>
+              <GridItem colStart={5}>
+                <Button>change</Button>
+              </GridItem>
             </Grid>
             <Grid templateColumns={'repeat(5, 1 fr)'}>
-              <GridItem colStart={1} >2FA</GridItem>
-              <GridItem colStart={5}><Switch></Switch></GridItem>
+              <GridItem colStart={1}>2FA</GridItem>
+              <GridItem colStart={5}>
+                <Switch></Switch>
+              </GridItem>
+            </Grid>
+            <Grid templateColumns={'repeat(5, 1 fr)'}>
+              <GridItem colStart={1}>Change avatar</GridItem>
+              <GridItem colStart={2}>
+                <InputFileUpload />
+              </GridItem>
             </Grid>
           </ModalBody>
           <ModalFooter>
             <Button onClick={onClose}>close</Button>
           </ModalFooter>
         </ModalContent>
-    </Modal>
+      </Modal>
     </div>
-
-  )
+  );
 }
-
