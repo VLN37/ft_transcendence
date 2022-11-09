3c8970e971d4a0b052e2938b3af5728fc6614e2c
VLN37 <jofelipe@student.42sp.org.br>
2022-11-09 08:07:37 -0300

    feat: hot reload
    this should make your life a lot easier

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -24,5 +26,11 @@ async function bootstrap() {
     }),
   );
   await app.listen(3000);
+
+  // hot reload related instructions
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
