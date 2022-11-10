13a11a86f32e51e7bcf628fc94aa3bd0f6d8befc
VLN37 <jofelipe@student.42sp.org.br>
2022-11-09 21:57:00 -0300

    chore: improve seeding scripts

--- back/scripts/db_seed.js
+++ back/scripts/db_seed.js
@@ -1,3 +1,4 @@
 const axios = require('axios').default;
 
 axios.get('http://localhost:3000/users/generate/' + 20).catch(() => {});
+axios.get('http://localhost:3000/channels/generate/' + 20).catch(() => {});
