2525614d06537a16b57b17851f858ad25ada1c15
wleite <wleite@student.42sp.org.br>
2022-11-11 00:11:23 -0300

    refactor: improve fake access_tokens generation

--- back/src/intra/intra.module.ts
+++ back/src/intra/intra.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { IntraServiceMock } from './intra.service.mock';
 import { IntraService } from './intra.service';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 // import { IntraService } from './intra.service';
 
 @Module({
+  imports: [JwtModule.register({}), UsersModule],
   providers: [
     {
       provide: IntraService,
--- back/src/intra/intra.service.mock.ts
+++ back/src/intra/intra.service.mock.ts
@@ -1,6 +1,10 @@
 import { faker } from '@faker-js/faker';
 import { Injectable, Logger } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { TokenPayload } from 'src/auth/dto/TokenPayload';
 import { IntraUser } from 'src/users/dto/intraUser.dto';
+import { UserDto } from 'src/users/dto/user.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class IntraServiceMock {
@@ -8,10 +12,33 @@ export class IntraServiceMock {
 
   private readonly users = {};
 
+  constructor(
+    private jwtService: JwtService,
+    private usersService: UsersService,
+  ) {}
+
   async getUserToken(code: string) {
+    let users: UserDto[] = await this.usersService.getAll();
+    console.log(users);
+    if (!users.length) {
+      await this.usersService.generateUsers(20);
+      users = await this.usersService.getAll();
+    }
+    const i = faker.datatype.number({
+      min: 1,
+      max: users.length,
+    });
+    const payload: TokenPayload = {
+      sub: users[i].id,
+      tfa_enabled: false,
+      is_authenticated_twice: false,
+    };
     this.logger.debug('faking user token');
     return {
-      access_token: faker.random.alpha(50),
+      access_token: this.jwtService.sign(payload, {
+        secret: process.env.JWT_SECRET,
+      }),
+      token_type: 'bearer',
     };
   }
 
