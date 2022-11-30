ed3e445ca4c487f4f7f46777f3ec931098c83da8
wleite <wleite@student.42sp.org.br>
2022-11-29 23:04:40 -0300

    feat: add notification badge

--- front/src/pages/Layout/index.tsx
+++ front/src/pages/Layout/index.tsx
@@ -1,16 +1,33 @@
-import { Container, Flex, Grid, GridItem } from '@chakra-ui/react';
+import { EmailIcon } from '@chakra-ui/icons';
+import {
+  Badge,
+  Container,
+  Flex,
+  Grid,
+  GridItem,
+  IconButton,
+} from '@chakra-ui/react';
 import { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import MatchFinder from '../../components/MatchFinder';
 import NeonButton from '../../components/NeonButton';
 import { Profile } from '../../components/Profile/profile';
+import { iDirectMessage } from '../../models/DirectMessage';
+import { emptyUser } from '../../models/User';
+import api from '../../services/api';
+import userStorage from '../../services/userStorage';
 
 import './style.css';
 
 export default function Layout({ setUser }: any) {
+  const user = userStorage.getUser() || emptyUser();
+  const [notification, setNotification] = useState(0);
   const [avatar, setAvatar] = useState<string>(
     JSON.parse(localStorage.getItem('user') || '').profile.avatar_path || '',
   );
+  api.subscribeDirectMessage((message: iDirectMessage) => {
+    if (message.sender.id != user.id) setNotification(notification + 1);
+  });
   return (
     <Container
       maxW="1200px"
@@ -48,8 +65,23 @@ export default function Layout({ setUser }: any) {
                 </Link>
               </li>
               <li>
-                <Link to="/dm">
-                  <NeonButton>DM</NeonButton>
+                <Link
+                  onClick={() => setNotification(0)}
+                  className="button"
+                  to="/dm"
+                >
+                  <IconButton
+                    variant="link"
+                    fontSize="50px"
+                    size="lg"
+                    aria-label="Message Notification"
+                    icon={<EmailIcon />}
+                  />
+                  {notification ? (
+                    <Badge className="badge" ml="1" colorScheme="green">
+                      {`+${notification}`}
+                    </Badge>
+                  ) : null}
                 </Link>
               </li>
             </ul>
