99dd86106d6ca3c0f10f2bab49925025fe0d02cc
VLN37 <jfpr1237@gmail.com>
2022-12-10 23:10:19 -0300

    refactor: proper state handing in DmUsers component

--- front/src/components/Chat/DmUsers.tsx
+++ front/src/components/Chat/DmUsers.tsx
@@ -33,16 +33,17 @@ const CircleIcon = (props: any) => (
 function UserDmMenu(props: {
   user: TableUser,
   user2: User,
-  reload: any,
-  setReload: any
+  me: User,
+  setMe: any
 }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const me: User = userStorage.getUser() || emptyUser();
   const toast = useToast();
   let navigate = useNavigate();
 
   async function blockUser() {
-    const response: any = await channelApi.blockUser(me.id, props.user.id);
+    const response: any = await channelApi.blockUser(
+      props.me.id, props.user.id
+    );
     const status = response.status == 201 ? 'success' : 'error';
     const message = response.status == 201 ? '' : response.data.message;
     toast({
@@ -50,40 +51,46 @@ function UserDmMenu(props: {
       status: status,
       description: message,
     })
-    await userStorage.updateUser();
-    props.setReload(!props.reload);
+    // await userStorage.updateUser();
+    props.me.blocked.push(props.user2);
+    userStorage.saveUser(props.me);
+    props.setMe({... props.me});
   }
 
   async function unblockUser() {
-    const response: any = await channelApi.unblockUser(me.id, props.user.id);
-    const status = response.status == 200 ? 'success' : 'error';
-    const message = response.status == 200 ? '' : response.data.message;
+    const resp: any = await channelApi.unblockUser(props.me.id, props.user.id);
+    const status = resp.status == 200 ? 'success' : 'error';
+    const message = resp.status == 200 ? '' : resp.data.message;
     toast({
       title: 'User unblock request sent',
       status: status,
       description: message,
     })
-    await userStorage.updateUser();
-    props.setReload(!props.reload);
+    if (resp.status == 200) {
+      const update = await userStorage.updateUser();
+      props.setMe({... update});
+    }
   }
 
   async function removeFriend() {
-    const response: any = await userApi.removeFriend(me.id, props.user.id);
-    const status = response.status == 200 ? 'success' : 'error';
-    const message = response.status == 200 ? '' : response.data.message;
+    const resp: any = await userApi.removeFriend(props.me.id, props.user.id);
+    const status = resp.status == 200 ? 'success' : 'error';
+    const message = resp.status == 200 ? '' : resp.data.message;
     toast({
       title: 'Friend removal request sent',
       status: status,
       description: message,
     })
-    if (response.status == 200) {
-      await userStorage.updateUser();
-      props.setReload(!props.reload);
+    if (resp.status == 200) {
+      const index = props.me.friends.findIndex(elem => elem.id == props.me.id);
+      props.me.friends.splice(index, 1);
+      userStorage.saveUser(props.me);
+      props.setMe({... props.me});
     }
   }
 
-  const isMyself = me.id == props.user.id;
-  const isBlocked = me.blocked.find((user) => props.user.id == user.id);
+  const isMyself = props.me.id == props.user.id;
+  const isBlocked = props.me.blocked.find((user) => props.user.id == user.id);
   let color;
   if (props.user.status == 'ONLINE')
     color = 'green.500';
@@ -128,33 +135,32 @@ function UserDmMenu(props: {
 function PendingRequestMenu(props: {
   user: TableUser,
   user2: User,
-  reload: any,
-  setReload: any,
+  me: User,
+  setMe: any,
 }) {
-  const me: User = userStorage.getUser() || emptyUser();
+  // const me: User = userStorage.getUser() || emptyUser();
 
   async function updateMe() {
-    me.friend_requests.splice(
-      me.friend_requests.findIndex(user => user.id == props.user.id), 1
+    props.me.friend_requests.splice(
+      props.me.friend_requests.findIndex(user => user.id == props.user.id), 1
     )
-    // userStorage.saveUser(me);
-    await userStorage.updateUser();
+    userStorage.saveUser(props.me);
+    props.setMe({... props.me})
+    // await userStorage.updateUser();
   }
 
   async function acceptFriend() {
-    const response: any = await userApi.acceptFriend(me.id, props.user.id);
-    if (response.status < 400) {
-      me.friends.push(props.user2);
+    const resp: any = await userApi.acceptFriend(props.me.id, props.user.id);
+    if (resp.status < 400) {
+      props.me.friends.push(props.user2);
       await updateMe();
-      props.setReload(!props.reload);
     }
   }
 
   async function rejectFriend() {
-    const response: any = await userApi.rejectFriend(me.id, props.user.id);
-    if (response.status < 400) {
+    const resp: any = await userApi.rejectFriend(props.me.id, props.user.id);
+    if (resp.status < 400) {
       await updateMe();
-      props.setReload(!props.reload);
     }
   }
 
@@ -189,16 +195,21 @@ export function DmUsers(props: {
   requests: User[]
 }) {
   const [reload, setReload] = useState<boolean>(false);
-  const me = userStorage.getUser() || emptyUser();
-
+  const [me, setMe] = useState<User>(
+    userStorage.getUser() || emptyUser()
+  );
+  const [requests, setRequests] = useState<User[]>(
+    userStorage.getUser()?.friend_requests || []
+  );
   async function updateFriends(data: any) {
     // for some reason this causes duplicate entries
     // me.friend_requests.push(data.user);
     // userStorage.saveUser(me);
 
     // console.log('callback called: ', data.user);
-    await userStorage.updateUser();
-    setReload(!reload);
+    me.friend_requests.push(data.user);
+    userStorage.saveUser(me);
+    setMe({... me});
   }
 
   useEffect(() => {
@@ -213,8 +224,8 @@ export function DmUsers(props: {
         key={i}
         user={tableuser}
         user2={user}
-        reload={reload}
-        setReload={setReload}
+        me={me}
+        setMe={setMe}
     ></UserDmMenu>
     );
   });
@@ -224,8 +235,8 @@ export function DmUsers(props: {
       key={i}
       user={tableuser}
       user2={user}
-      reload={reload}
-      setReload={setReload}
+      me={me}
+      setMe={setMe}
     />
   })
   return (
