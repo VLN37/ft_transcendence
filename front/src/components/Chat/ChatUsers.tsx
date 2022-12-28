a954bf29d5d4527b2ab087679bc4a3ef978e2ad2
VLN37 <jfpr1237@gmail.com>
2022-12-27 22:06:50 -0300

    chore: file formatting

--- front/src/components/Chat/ChatUsers.tsx
+++ front/src/components/Chat/ChatUsers.tsx
@@ -11,7 +11,6 @@ import {
   Text,
 } from '@chakra-ui/react';
 import { useState } from 'react';
-import { isBlock } from 'typescript';
 import { Channel } from '../../models/Channel';
 import { TableUser } from '../../models/TableUser';
 import { emptyUser, User } from '../../models/User';
@@ -20,51 +19,57 @@ import userStorage from '../../services/userStorage';
 import { PublicProfile } from '../Profile/profile.public';
 
 const CircleIcon = (props: any) => (
-  <Icon viewBox="0 0 200 200" {...props}>
+  <Icon viewBox='0 0 200 200' {...props}>
     <path
-      fill="currentColor"
-      d="M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0"
+      fill='currentColor'
+      d='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
     />
   </Icon>
-);
+)
 
-function UserMenu(props: { channel: Channel; user: TableUser; user2: User }) {
+function UserMenu(props: {
+  channel: Channel,
+  user: TableUser,
+  user2: User,
+}) {
   const [reload, setReload] = useState<boolean>(false);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const me: User = userStorage.getUser() || emptyUser();
   const toast = useToast();
 
   async function blockUser() {
-    const response: any = await channelApi.blockUser(me.id, props.user.id);
-	if (!response) return;
+    const response: any = await channelApi.blockUser(
+      me.id, props.user.id
+    );
     const status = response.status == 201 ? 'success' : 'error';
     const message = response.status == 201 ? '' : response.data.message;
     toast({
       title: 'User block request sent',
       status: status,
       description: message,
-    });
+    })
     await userStorage.updateUser();
     setReload(!reload);
   }
 
   async function unblockUser() {
-    const response: any = await channelApi.unblockUser(me.id, props.user.id);
+    const response: any = await channelApi.unblockUser(
+      me.id, props.user.id
+    );
     const status = response.status == 200 ? 'success' : 'error';
     const message = response.status == 200 ? '' : response.data.message;
     toast({
       title: 'User unblock request sent',
       status: status,
       description: message,
-    });
+    })
     await userStorage.updateUser();
     setReload(!reload);
   }
 
   async function delAdmin() {
     const response: any = await channelApi.delAdmin(
-      props.user.id,
-      props.channel.id,
+      props.user.id, props.channel.id
     );
     const status = response.status == 200 ? 'success' : 'error';
     const message = response.status == 200 ? '' : response.data.message;
@@ -72,20 +77,19 @@ function UserMenu(props: { channel: Channel; user: TableUser; user2: User }) {
       title: 'Delete admin request sent',
       status: status,
       description: message,
-    });
+    })
     if (response.status == 200)
-      props.channel.admins.splice(
-        props.channel.admins.findIndex((user) => user.id == props.user.id),
+      props.channel.admins
+        .splice(props.channel.admins
+        .findIndex((user) => user.id == props.user.id)
       );
     setReload(!reload);
   }
 
   async function addAdmin() {
     const response: any = await channelApi.addAdmin(
-      props.user.id,
-      props.channel.id,
+      props.user.id, props.channel.id
     );
-	if (!response) return;
     const status = response.status == 201 ? 'success' : 'error';
     const message = response.status == 201 ? '' : response.data.message;
     toast({
@@ -93,35 +97,33 @@ function UserMenu(props: { channel: Channel; user: TableUser; user2: User }) {
       status: status,
       description: message,
     });
-    if (response.status == 201) props.channel.admins.push(props.user2);
+    if (response.status == 201)
+      props.channel.admins.push(props.user2);
     setReload(!reload);
   }
 
   async function banUser() {
     const response: any = await channelApi.banUser(
-      props.channel.id,
-      props.user.id,
-      360,
-    );
-    if (!response) return;
+      props.channel.id, props.user.id, 360
+      );
     const status = response.status == 201 ? 'success' : 'error';
-    const message =
-      response.status == 201 ? '5 minutes timeout' : response.data.message;
+    const message = response.status == 201
+      ? '5 minutes timeout'
+      : response.data.message;
     toast({
       title: 'User ban request sent',
       status: status,
       description: message,
     });
-    if (response.status == 201) props.channel.banned_users.push(props.user.id);
+    if (response.status == 201)
+      props.channel.banned_users.push(props.user.id);
     setReload(!reload);
   }
 
   async function unbanUser() {
     const response: any = await channelApi.unbanUser(
-      props.channel.id,
-      props.user.id,
+      props.channel.id, props.user.id
     );
-	if (!response) return;
     const status = response.status == 200 ? 'success' : 'error';
     const message = response.status == 200 ? '' : response.data.message;
     toast({
@@ -131,22 +133,24 @@ function UserMenu(props: { channel: Channel; user: TableUser; user2: User }) {
     });
     if (response.status == 200)
       props.channel.banned_users.splice(
-        props.channel.banned_users.indexOf(props.user.id),
-        1,
+        props.channel.banned_users.indexOf(props.user.id), 1
       );
     setReload(!reload);
   }
 
   const isMyself = me.id == props.user.id;
   const amOwner = me.id == props.channel.owner_id && me.id != props.user.id;
-  const isBlocked = me.blocked.find((user) => props.user.id == user.id);
-  const isAdmin = props.channel.admins.find((user) => props.user.id == user.id);
-  const amAdmin = props.channel.admins.find((user) => me.id == user.id);
-  const isBanned = props.channel.banned_users.find((i) => i.user_id == props.user.id);
+  const isBlocked = me.blocked.find(user => props.user.id == user.id);
+  const isAdmin = props.channel.admins.find(user => props.user.id == user.id);
+  const amAdmin = props.channel.admins.find(user => me.id == user.id);
+  const isBan = props.channel.banned_users.find(i => i.user_id == props.user.id);
   let color;
-  if (props.user.status == 'ONLINE') color = 'green.500';
-  else if (props.user.status == 'OFFLINE') color = 'grey.500';
-  else color = 'red.500';
+  if (props.user.status == 'ONLINE')
+    color = 'green.500';
+  else if (props.user.status == 'OFFLINE')
+    color = 'grey.500';
+  else
+    color = 'red.500';
   return (
     <Box padding={1}>
       <PublicProfile
@@ -155,7 +159,7 @@ function UserMenu(props: { channel: Channel; user: TableUser; user2: User }) {
         onClose={onClose}
       ></PublicProfile>
       <Menu isLazy>
-        <MenuButton color={isBanned ? 'red' : 'white'}>
+        <MenuButton color={isBan ? 'red' : 'white'}>
           <CircleIcon color={color}></CircleIcon>
           {isBlocked ? <ViewOffIcon></ViewOffIcon> : null}
           {isAdmin ? <StarIcon></StarIcon> : null}
@@ -164,25 +168,32 @@ function UserMenu(props: { channel: Channel; user: TableUser; user2: User }) {
         <MenuList>
           <MenuItem onClick={onOpen}>view profile</MenuItem>
           <MenuItem>invite to game</MenuItem>
-          {isMyself ? null : isBlocked ? (
-            <MenuItem onClick={unblockUser}>unblock user</MenuItem>
-          ) : (
-            <MenuItem onClick={blockUser}>block user</MenuItem>
-          )}
-          {amOwner && !isMyself ? (
-            isAdmin ? (
-              <MenuItem onClick={delAdmin}>remove admin</MenuItem>
-            ) : (
-              <MenuItem onClick={addAdmin}>give admin</MenuItem>
+          {
+            isMyself
+              ? null
+              : (
+              isBlocked
+                ? <MenuItem onClick={unblockUser}>unblock user</MenuItem>
+                : <MenuItem onClick={blockUser}>block user</MenuItem>
+              )
+          }
+          {
+            amOwner && !isMyself
+              ? (
+            isAdmin
+              ? <MenuItem onClick={delAdmin}>remove admin</MenuItem>
+              : <MenuItem onClick={addAdmin}>give admin</MenuItem>
+          ) : null
+          }
+          {
+            amAdmin && !isMyself
+              ? (
+              isBan
+                ? <MenuItem onClick={unbanUser}>unban user</MenuItem>
+                : <MenuItem onClick={banUser}>ban user</MenuItem>
             )
-          ) : null}
-          {amAdmin && !isMyself ? (
-            isBanned ? (
-              <MenuItem onClick={unbanUser}>unban user</MenuItem>
-            ) : (
-              <MenuItem onClick={banUser}>ban user</MenuItem>
-            )
-          ) : null}
+              : null
+          }
         </MenuList>
       </Menu>
     </Box>
@@ -203,9 +214,7 @@ export function ChatUsers(props: { channel: Channel }) {
   });
   return (
     <>
-      <Text as={'b'} paddingLeft={1}>
-        Channel members
-      </Text>
+      <Text paddingLeft={1}>Channel members</Text>
       <hr></hr>
       {userList}
     </>
