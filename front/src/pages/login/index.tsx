65528e2fab4717cc49c0264de2c44fafc5cba948
VLN37 <jfpr1237@gmail.com>
2023-01-06 17:08:04 -0300

    style: new login page

--- front/src/pages/login/index.tsx
+++ front/src/pages/login/index.tsx
@@ -1,24 +1,55 @@
-import { Box } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import { Box, VStack, Link } from '@chakra-ui/react';
 import NeonButton from '../../components/NeonButton';
 
 export default function LoginPage({ user }: any) {
   const authorizationUrl = process.env.REACT_APP_INTRA_AUTHORIZATION_URL;
 
+  const link = process.env.REACT_APP_FRONT_HOSTNAME + '/auth-callback?';
   return (
-    <Box paddingX="10%">
-      <h2>Welcome to PONG Transcendence</h2>
-      <p>Log in on 42 Intranet to access the game</p>
-      {user ? (
+    <VStack marginTop={"15%"}justifyItems={'center'} spacing={'24px'}>
+      {/* <Box>
+        <p >Welcome to PONG Transcendence</p>
+        <p >Log in on 42 Intranet to access the game</p>
+      </Box> */}
+      {/* <Box>
         <Link to="/">
           <NeonButton>GO HOME</NeonButton>
         </Link>
-      ) : (
-        <div>
-          <a href={authorizationUrl!}>
-            <NeonButton>LOGIN</NeonButton>
-          </a>
-          <br />
+      </Box> */}
+      <Box>
+        <Link href={authorizationUrl!}>
+          <NeonButton>LOGIN AS 42</NeonButton>
+        </Link>
+      </Box>
+      <Box>
+        <Link href={link + 'code=guest'}>
+          <NeonButton>LOGIN AS GUEST</NeonButton>
+        </Link>
+      </Box>
+      <Box>
+        <Link href={link + 'code=abcd'}>
+          <NeonButton>PAULO</NeonButton>
+        </Link>
+      </Box>
+      <Box>
+        <Link href={link + 'code=1234'}>
+          <NeonButton>JOAO</NeonButton>
+        </Link>
+      </Box>
+      <Box>
+        <Link href={link + 'code=noob'}>
+          <NeonButton>WELTON</NeonButton>
+        </Link>
+      </Box>
+    </VStack>
+  );
+}
+{
+  /* <Box>
+            <a href={authorizationUrl!}>
+              <NeonButton>LOGIN</NeonButton>
+            </a>
+          </Box>
           <a href={
             process.env.REACT_APP_FRONT_HOSTNAME + "/auth-callback?code=abcd"
           }>
@@ -36,14 +67,5 @@ export default function LoginPage({ user }: any) {
           }>
             <NeonButton>login as welton</NeonButton>
           </a>
-          <br />
-          <a href={
-            process.env.REACT_APP_FRONT_HOSTNAME + "/auth-callback?code=guest"
-          }>
-            <NeonButton>LOGIN AS GUEST</NeonButton>
-          </a>
-        </div>
-      )}
-    </Box>
-  );
+      )} */
 }
