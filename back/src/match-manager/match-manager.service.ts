1325cd06ab3b9097685251e15f40762b5fa39f41
Paulo Santana <paulo.santana.r@gmail.com>
2023-01-08 18:36:00 -0300

    refactor: query once for last matches

--- back/src/match-manager/match-manager.service.ts
+++ back/src/match-manager/match-manager.service.ts
@@ -55,23 +55,11 @@ export class MatchManagerService {
   async getLiveMatches(qty: number): Promise<Match[]> {
     let finishedMatches = [];
     const liveMatches = await this.matchRepository.find({
-      where: {
-        stage: 'ONGOING',
-      },
+      where: [{ stage: 'ONGOING' }, { stage: 'FINISHED' }],
       relations: ['left_player.profile', 'right_player.profile'],
       take: qty,
-      order: { created_at: 'DESC' },
+      order: { stage: 'ASC', created_at: 'DESC' },
     });
-    if (liveMatches.length < qty) {
-      finishedMatches = await this.matchRepository.find({
-        where: {
-          stage: 'FINISHED',
-        },
-        relations: ['left_player.profile', 'right_player.profile'],
-        take: qty - liveMatches.length,
-        order: { created_at: 'DESC' },
-      });
-    }
     const matches = liveMatches.concat(finishedMatches);
     this.logger.debug('Returning live matches');
     return matches;
