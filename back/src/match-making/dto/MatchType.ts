20479ed633730350f32c708c44c2125ca0591c47
Paulo Santana <paulo.santana.r@gmail.com>
2023-01-12 23:34:43 -0300

    chore: remove unused type

--- back/src/match-making/dto/MatchType.ts
+++ back/src/match-making/dto/MatchType.ts
@@ -3,7 +3,3 @@ export const MATCH_TYPES = ['CLASSIC', 'TURBO'] as const;
 type MatchTypeTuple = typeof MATCH_TYPES;
 
 export type MatchType = MatchTypeTuple[number];
-
-export interface AppendToQueueDTO {
-  type: MatchType;
-}
