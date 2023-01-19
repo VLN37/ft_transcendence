3c5d92b9d514d4d4d827775eef5b8d11b0c499fa
Paulo Santana <paulo.santana.r@gmail.com>
2023-01-19 17:59:09 -0300

    feat: add j and k as control keys

--- front/src/game/controls.ts
+++ front/src/game/controls.ts
@@ -3,17 +3,23 @@ import { GameApi } from '../services/gameApi';
 import { PlayerCommand } from './model/PlayerCommand';
 
 export const handleKeyPress = (p5: p5Types, gameApi: GameApi) => {
-  if (p5.keyCode == p5.UP_ARROW || p5.key.toLowerCase() == 'w') {
+  if (p5.keyCode == p5.UP_ARROW || 'kw'.includes(p5.key.toLowerCase())) {
     gameApi.issueCommand(PlayerCommand.MOVE_UP);
-  } else if (p5.keyCode == p5.DOWN_ARROW || p5.key.toLowerCase() == 's') {
+  } else if (
+    p5.keyCode == p5.DOWN_ARROW ||
+    'js'.includes(p5.key.toLowerCase())
+  ) {
     gameApi.issueCommand(PlayerCommand.MOVE_DOWN);
   }
 };
 
 export const handleKeyRelease = (p5: p5Types, gameApi: GameApi) => {
-  if (p5.keyCode == p5.UP_ARROW || p5.key.toLowerCase() == 'w') {
+  if (p5.keyCode == p5.UP_ARROW || 'kw'.includes(p5.key.toLowerCase())) {
     gameApi.issueCommand(PlayerCommand.STOP_MOVE_UP);
-  } else if (p5.keyCode == p5.DOWN_ARROW || p5.key.toLowerCase() == 's') {
+  } else if (
+    p5.keyCode == p5.DOWN_ARROW ||
+    'js'.includes(p5.key.toLowerCase())
+  ) {
     gameApi.issueCommand(PlayerCommand.STOP_MOVE_DOWN);
   }
 };
